02adea29bd7341eb95fa60c2b03d814d47e859e0
Simon Greenwood <simongreenwood90@gmail.com>
2016-03-19 11:17:57 +0000

    Setup and styled basic project functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317200356) do
+ActiveRecord::Schema.define(version: 20160318192946) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -22,5 +22,13 @@ ActiveRecord::Schema.define(version: 20160317200356) do
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
